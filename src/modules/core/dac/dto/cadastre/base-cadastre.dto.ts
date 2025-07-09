a9d5ccc6623c73d18c7a83f2e94d960bffa70dc1
cmtl0204 <cesar.tamayo0204@gmail.com>
2025-07-09 17:34:41 -0500

    add migrations

--- src/modules/core/dac/dto/cadastre/base-cadastre.dto.ts
+++ src/modules/core/dac/dto/cadastre/base-cadastre.dto.ts
@@ -5,27 +5,35 @@ import {
   isStringValidationOptions,
 } from '../../../../../utils/dto-validation';
 import { ProcessEntity } from '@modules/core/entities';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BaseCadastreDto {
+  @ApiProperty()
   @IsOptional()
   readonly process: ProcessEntity;
 
+  @ApiProperty()
   @IsNotEmpty(isNotEmptyValidationOptions())
   readonly idTemp: number;
 
+  @ApiProperty()
   @IsString(isStringValidationOptions())
   readonly idProcess: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString(isStringValidationOptions())
   readonly observation: string;
 
+  @ApiProperty()
   @IsString(isStringValidationOptions())
   readonly registerNumber: string;
 
+  @ApiProperty()
   @IsDate(isDateValidationOptions())
   readonly registeredAt: Date;
 
+  @ApiProperty()
   @IsString(isStringValidationOptions())
   readonly systemOrigin: string;
 }

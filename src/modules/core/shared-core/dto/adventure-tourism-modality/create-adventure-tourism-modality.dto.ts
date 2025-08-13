82e670853343c1413b8036dcb7b797e443d651cc
cmtl0204 <cesar.tamayo0204@gmail.com>
2025-08-12 21:57:30 -0500

    fix bugs

--- src/modules/core/shared-core/dto/adventure-tourism-modality/create-adventure-tourism-modality.dto.ts
+++ src/modules/core/shared-core/dto/adventure-tourism-modality/create-adventure-tourism-modality.dto.ts
@@ -1,8 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsObject, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CatalogueDto } from '@modules/common/catalogue/dto';
 
 export class CreateAdventureTourismModalityDto {
+  @IsObject()
   // @ValidateNested()
   @Type(() => CatalogueDto)
   readonly type: CatalogueDto;
